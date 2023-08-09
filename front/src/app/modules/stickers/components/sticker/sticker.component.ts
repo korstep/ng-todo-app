07a469ed4a3b911009b3af61d54edf7f6fc3596f
Steve <kornilov.stepan11.s06@gmail.com>
2023-08-09 13:06:44 +0300

    Implement stickers work

--- front/src/app/modules/stickers/components/sticker/sticker.component.ts
+++ front/src/app/modules/stickers/components/sticker/sticker.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ISticker } from 'src/app/core/interfaces/sticker.interface';
 
 @Component({
   selector: 'app-sticker',
   templateUrl: './sticker.component.html',
-  styleUrls: ['./sticker.component.scss']
+  styleUrls: ['./sticker.component.scss'],
 })
 export class StickerComponent {
-
+  @Input()
+  sticker!: ISticker;
 }
--- front/src/app/modules/stickers/components/stickers-list/stickers-list.component.ts
+++ front/src/app/modules/stickers/components/stickers-list/stickers-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StickyWallService } from '../../services/stickers.service';
+import { ISticker } from 'src/app/core/interfaces/sticker.interface';
 
 @Component({
   selector: 'app-stickers-list',
   templateUrl: './stickers-list.component.html',
-  styleUrls: ['./stickers-list.component.scss']
+  styleUrls: ['./stickers-list.component.scss'],
 })
-export class StickersListComponent {
+export class StickersListComponent implements OnInit {
+  constructor(private stickyService: StickyWallService) {}
 
+  stickers: ISticker[] = [];
+
+  ngOnInit() {
+    this.stickers = this.stickyService.getAll();
+  }
 }
