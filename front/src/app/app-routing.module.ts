50827ee297963c4cfc8c1f4d6cb25442bcca7d7f
Steve <kornilov.stepan11.s06@gmail.com>
2023-08-09 12:43:25 +0300

    Update app components

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticatedLayoutComponent } from './layouts/authenticated-layout/authenticated-layout.component';
+import { UnauthenticatedLayoutComponent } from './layouts/unauthenticated-layout/unauthenticated-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // App routes
+  {
+    path: '',
+    component: AuthenticatedLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks',
+        loadChildren: () =>
+          import('./modules/tasks/tasks.module').then((m) => m.TasksModule),
+      },
+      {
+        path: 'stickers',
+        loadChildren: () =>
+          import('./modules/stickers/stickers.module').then(
+            (m) => m.StickersModule
+          ),
+      },
+    ],
+  },
+  { path: '', component: UnauthenticatedLayoutComponent, children: [] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
