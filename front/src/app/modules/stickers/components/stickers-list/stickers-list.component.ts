c79130a9fdea12923ad7e56d321fd24e519a33d0
Steve <kornilov.stepan11.s06@gmail.com>
2023-08-09 12:48:15 +0300

    Create stickers-list components

---
+++ front/src/app/modules/stickers/components/stickers-list/stickers-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stickers-list',
+  templateUrl: './stickers-list.component.html',
+  styleUrls: ['./stickers-list.component.scss']
+})
+export class StickersListComponent {
+
+}
