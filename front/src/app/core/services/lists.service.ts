681ab124d72d6ab120e79341ed95fc5e7d55e295
Steve <kornilov.stepan11.s06@gmail.com>
2023-08-10 20:01:34 +0300

    Implement lists service

---
+++ front/src/app/core/services/lists.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject, catchError, of, tap } from 'rxjs';
+import { IList } from 'src/app/core/interfaces/list.interface';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ListsService {
+  constructor(private http: HttpClient) {}
+
+  private listsChangesSubject = new Subject<void>();
+
+  get listsChanges$(): Observable<void> {
+    return this.listsChangesSubject.asObservable();
+  }
+
+  addList(list: IList): Observable<any> {
+    return this.http.post('http://localhost:3000/lists', list).pipe(
+      tap(() => {
+        this.listsChangesSubject.next();
+      }),
+      catchError((err) => {
+        console.log(`error:  ${err}`);
+        return of();
+      })
+    );
+  }
+
+  getAll(): Observable<IList[]> {
+    return this.http.get<IList[]>('http://localhost:3000/lists');
+  }
+}
