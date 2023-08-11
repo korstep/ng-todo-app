1e4007d549769b6df4c2fb9a131f87ce3d2df688
Steve <kornilov.stepan11.s06@gmail.com>
2023-08-11 10:34:15 +0300

    Implement work with tasks service

--- front/src/app/modules/tasks/components/tasks-list/tasks-list.component.ts
+++ front/src/app/modules/tasks/components/tasks-list/tasks-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TasksService } from '../../services/tasks.service';
 import { ITask } from 'src/app/core/interfaces/task.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tasks-list',
@@ -8,10 +9,14 @@ import { ITask } from 'src/app/core/interfaces/task.interface';
   styleUrls: ['./tasks-list.component.scss'],
 })
 export class TasksListComponent implements OnInit {
+  tasks$!: Observable<ITask[]>;
   constructor(private tasksService: TasksService) {}
 
-  tasks: ITask[] = [];
-  ngOnInit(): void {
-    this.tasks = this.tasksService.getAll();
+  ngOnInit() {
+    this.tasks$ = this.tasksService.getAll();
+
+    this.tasksService.tasksChanges$.subscribe(() => {
+      this.tasks$ = this.tasksService.getAll();
+    });
   }
 }
