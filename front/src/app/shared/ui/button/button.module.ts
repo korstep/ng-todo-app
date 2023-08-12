58b5e9d322850847b4d17c1e0f01393560126654
Steve <kornilov.stepan11.s06@gmail.com>
2023-08-12 17:03:58 +0300

    Create ui component add-button

--- front/src/app/shared/ui/button/button.module.ts
+++ front/src/app/shared/ui/button/button.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AcceptButtonComponent } from './accept-button/accept-button.component';
-
-
+import { AddButtonComponent } from './add-button/add-button.component';
 
 @NgModule({
-  declarations: [
-    AcceptButtonComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AcceptButtonComponent, AddButtonComponent],
+  exports: [AcceptButtonComponent, AddButtonComponent],
+  imports: [CommonModule],
 })
-export class ButtonModule { }
+export class ButtonModule {}
