d0be94aaaa50adfcbd24efedd3a273edc147a1f4
Steve <kornilov.stepan11.s06@gmail.com>
2023-08-12 17:06:35 +0300

    Use ui component add-button

--- front/src/app/modules/tasks/tasks.module.ts
+++ front/src/app/modules/tasks/tasks.module.ts
@@ -6,10 +6,22 @@ import { TasksComponent } from './tasks.component';
 import { TasksListComponent } from './components/tasks-list/tasks-list.component';
 import { TaskComponent } from './components/task/task.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { ListModule } from '@ui';
+import { ListModule, ButtonModule } from '@ui';
+import { AddTaskButtonComponent } from './components/add-task-button/add-task-button.component';
 
 @NgModule({
-  declarations: [TasksComponent, TasksListComponent, TaskComponent],
-  imports: [CommonModule, TasksRoutingModule, SharedModule, ListModule],
+  declarations: [
+    TasksComponent,
+    TasksListComponent,
+    TaskComponent,
+    AddTaskButtonComponent,
+  ],
+  imports: [
+    CommonModule,
+    TasksRoutingModule,
+    SharedModule,
+    ListModule,
+    ButtonModule,
+  ],
 })
 export class TasksModule {}
