bb00a5f2daed6ee9c315320081bba634a512001c
Steve <kornilov.stepan11.s06@gmail.com>
2023-08-16 21:01:57 +0300

    Create list path

--- front/src/app/modules/tasks/components/tasks-list/tasks-list.component.ts
+++ front/src/app/modules/tasks/components/tasks-list/tasks-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TasksService } from '../../services/tasks.service';
 import { ITask } from 'src/app/core/interfaces/task.interface';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tasks-list',
@@ -8,12 +9,34 @@ import { ITask } from 'src/app/core/interfaces/task.interface';
   styleUrls: ['./tasks-list.component.scss'],
 })
 export class TasksListComponent implements OnInit {
-  tasks: ITask[] = [];
-  constructor(private tasksService: TasksService) {}
+  private tasks: ITask[] = [];
+  private selectedList: number | undefined;
+  filteredTasks: ITask[] = [];
+  constructor(
+    private tasksService: TasksService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.tasksService.getAll().subscribe((tasks) => {
       this.tasks = tasks;
+      this.updateFilteredTasks();
     });
+    this.subscribeOnList();
+  }
+  subscribeOnList() {
+    this.route.params.subscribe((params) => {
+      this.selectedList = Number(params['list']);
+      this.updateFilteredTasks();
+    });
+  }
+  updateFilteredTasks() {
+    if (this.selectedList) {
+      this.filteredTasks = this.tasks.filter(
+        (item) => item.list.id === this.selectedList
+      );
+    } else {
+      this.filteredTasks = this.tasks;
+    }
   }
 }
