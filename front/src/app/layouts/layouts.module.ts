0acff9e50a7f5d158bddfa0f8818fc5eed83d700
Steve <kornilov.stepan11.s06@gmail.com>
2023-08-19 19:03:32 +0300

    Update layouts.module.ts

--- front/src/app/layouts/layouts.module.ts
+++ front/src/app/layouts/layouts.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticatedLayoutComponent } from './authenticated-layout/authenticated-layout.component';
-import { UnauthenticatedLayoutComponent } from './unauthenticated-layout/unauthenticated-layout.component';
 import { CoreModule } from '../core/core.module';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AuthenticatedLayoutComponent, UnauthenticatedLayoutComponent],
-  exports: [AuthenticatedLayoutComponent, UnauthenticatedLayoutComponent],
+  declarations: [AuthenticatedLayoutComponent],
+  exports: [AuthenticatedLayoutComponent],
   imports: [RouterModule, CommonModule, CoreModule],
 })
 export class LayoutsModule {}
