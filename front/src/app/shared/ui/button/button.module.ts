6b4ecb01067ace10ba309f43528143ebb0a3aea5
Steve <kornilov.stepan11.s06@gmail.com>
2023-08-19 21:32:32 +0300

    Replace accept button component by mixin

--- front/src/app/shared/ui/button/button.module.ts
+++ front/src/app/shared/ui/button/button.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AcceptButtonComponent } from './accept-button/accept-button.component';
 import { AddButtonComponent } from './add-button/add-button.component';
 
 @NgModule({
-  declarations: [AcceptButtonComponent, AddButtonComponent],
-  exports: [AcceptButtonComponent, AddButtonComponent],
+  declarations: [AddButtonComponent],
+  exports: [AddButtonComponent],
   imports: [CommonModule],
 })
 export class ButtonModule {}
