c819766cb443486b06a8e8d98e8e749596f773f2
Steve <kornilov.stepan11.s06@gmail.com>
2023-08-20 15:26:14 +0300

    Update app.module.ts

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutsModule } from './layouts/layouts.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutsModule],
+  imports: [BrowserModule, AppRoutingModule, LayoutsModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })
