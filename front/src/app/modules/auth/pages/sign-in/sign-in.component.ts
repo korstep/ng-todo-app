cc38a93af16a871fb62efcbb7dd9e38707aea86f
Steve <kornilov.stepan11.s06@gmail.com>
2023-08-24 13:55:33 +0300

    Update sign-in.component.ts

--- front/src/app/modules/auth/pages/sign-in/sign-in.component.ts
+++ front/src/app/modules/auth/pages/sign-in/sign-in.component.ts
@@ -1,25 +1,25 @@
-import { Component } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  NgForm,
-  Validators,
-} from '@angular/forms';
+import { Component, OnDestroy } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
 })
-export class SignInComponent {
+export class SignInComponent implements OnDestroy {
   title: string = 'Sign in';
   signInForm!: FormGroup;
   emailFormControl!: FormControl;
   passwordControl: FormControl;
   submitted: boolean = false;
+  private signinSubscription: Subscription | undefined;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {
     this.signInForm = this.formBuilder.group({
       email: [''],
       password: [''],
@@ -31,8 +31,23 @@ export class SignInComponent {
 
   onSubmit() {
     if (this.signInForm.valid) {
-      console.log(this.signInForm);
+      this.signinSubscription = this.authService
+        .signin(
+          this.signInForm.controls['email'].value,
+          this.signInForm.controls['password'].value
+        )
+        .subscribe({
+          next: () => {},
+          error: (error) => {
+            console.error(error);
+          },
+        });
     }
     this.submitted = true;
+  }
+  ngOnDestroy() {
+    if (this.signinSubscription) {
+      this.signinSubscription.unsubscribe();
+    }
   }
 }
