0f41c768038c98c4260dc50b3f03261393de9a64
Steve <kornilov.stepan11.s06@gmail.com>
2023-08-24 13:55:20 +0300

    Update sign-up.component.ts

--- front/src/app/modules/auth/pages/sign-up/sign-up.component.ts
+++ front/src/app/modules/auth/pages/sign-up/sign-up.component.ts
@@ -1,34 +1,55 @@
-import { Component } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  FormControl,
-} from '@angular/forms';
+import { Component, OnDestroy } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnDestroy {
   title: string = 'Sign Up';
-  signInForm!: FormGroup;
+  signUpForm!: FormGroup;
   emailFormControl!: FormControl;
   passwordControl: FormControl;
   submitted: boolean = false;
+  private signupSubscription: Subscription | undefined;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.signInForm = this.formBuilder.group({
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {
+    this.signUpForm = this.formBuilder.group({
       email: [''],
       password: [''],
     });
 
-    this.emailFormControl = this.signInForm.get('email') as FormControl;
-    this.passwordControl = this.signInForm.get('password') as FormControl;
+    this.emailFormControl = this.signUpForm.get('email') as FormControl;
+    this.passwordControl = this.signUpForm.get('password') as FormControl;
   }
 
   onSubmit() {
-    console.log(this.signInForm);
+    if (this.signUpForm.valid) {
+      console.log(this.signUpForm);
+      this.signupSubscription = this.authService
+        .signup(
+          this.signUpForm.controls['email'].value,
+          this.signUpForm.controls['password'].value
+        )
+        .subscribe({
+          next: () => {},
+          error: (error) => {
+            console.error(error);
+          },
+        });
+    }
+    this.submitted = true;
+  }
+
+  ngOnDestroy() {
+    if (this.signupSubscription) {
+      this.signupSubscription.unsubscribe();
+    }
   }
 }
