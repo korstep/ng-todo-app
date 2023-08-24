a02233beed27d3efb275ddefac5875b88bdf77ae
Steve <kornilov.stepan11.s06@gmail.com>
2023-08-24 13:55:15 +0300

    Implement auth service

--- front/src/app/modules/auth/services/auth.service.ts
+++ front/src/app/modules/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { BACKEND_URl } from '@constants';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { ISuccessfulLoggin } from '@interfaces/auth.interface';
 
 @Injectable({
@@ -10,13 +10,70 @@ import { ISuccessfulLoggin } from '@interfaces/auth.interface';
 })
 export class AuthService {
   private readonly tokenCookieName = 'access_token';
-  private isUserLoggedIn: boolean = false;
 
   constructor(private http: HttpClient, private cookieService: CookieService) {}
 
-  signin(email: string, password: string) {}
+  isUserLogged(): boolean {
+    return this.getAccessToken() !== '';
+  }
 
-  signup(email: string, password: string) {}
+  private setAccessTokenToCookie(token: string): void {
+    const expirationDate = new Date();
+    expirationDate.setTime(expirationDate.getTime() + 1 * 60 * 60 * 1000);
+    this.cookieService.set(this.tokenCookieName, token, expirationDate);
+  }
 
-  signout() {}
+  private getAccessToken(): string {
+    return this.cookieService.get(this.tokenCookieName);
+  }
+
+  private isSuccessfulResponse(response: any): response is ISuccessfulLoggin {
+    return (
+      response && typeof response === 'object' && 'accessToken' in response
+    );
+  }
+
+  signin(email: string, password: string): Observable<boolean> {
+    return this.http
+      .post<ISuccessfulLoggin>(`${BACKEND_URl}/signin`, {
+        email: email,
+        password: password,
+      })
+      .pipe(
+        map((response: ISuccessfulLoggin | string) => {
+          if (this.isSuccessfulResponse(response)) {
+            this.setAccessTokenToCookie(response.accessToken);
+            return true;
+          }
+          return false;
+        }),
+        catchError((error) => {
+          return of(error);
+        })
+      );
+  }
+
+  signup(email: string, password: string): Observable<boolean> {
+    return this.http
+      .post<ISuccessfulLoggin>(`${BACKEND_URl}/signup`, {
+        email: email,
+        password: password,
+      })
+      .pipe(
+        map((response: ISuccessfulLoggin | string) => {
+          if (this.isSuccessfulResponse(response)) {
+            this.setAccessTokenToCookie(response.accessToken);
+            return true;
+          }
+          return false;
+        }),
+        catchError((error) => {
+          return of(error);
+        })
+      );
+  }
+
+  signout(): void {
+    this.cookieService.delete(this.tokenCookieName);
+  }
 }
