c56b93a8fb58e26838d16db5fda7b51ee1ef24dd
Steve <kornilov.stepan11.s06@gmail.com>
2023-08-25 14:26:18 +0300

    Update app-routing.module.ts

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticatedLayoutComponent } from '@layouts/authenticated-layout/authenticated-layout.component';
+import { AuthGuard } from './core/guards/auth.guard';
+import { AppGuard } from './core/guards/app.guard';
 
 const routes: Routes = [
   // AUTH
   {
     path: '',
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
@@ -13,6 +16,7 @@ const routes: Routes = [
   {
     path: '',
     component: AuthenticatedLayoutComponent,
+    canActivate: [AppGuard],
     children: [
       {
         path: '',
